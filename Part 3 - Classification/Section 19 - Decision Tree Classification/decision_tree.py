758897ce00d29cf0082b2952c7abb62b9494f6c2
Tanuj Vishnoi <vishnoitanuj@outlook.com>
2018-11-08 23:02:43 +0530

    Random Forest Complete

--- Part 3 - Classification/Section 19 - Decision Tree Classification/decision_tree.py
+++ Part 3 - Classification/Section 19 - Decision Tree Classification/decision_tree.py
@@ -1,5 +1,3 @@
-# Naive Bayes
-
 # Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
@@ -20,9 +18,9 @@ sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
-# Fitting Naive Bayes to the Training set
-from sklearn.tree import DecisionTreeClassifier
-classifier = DecisionTreeClassifier(criterion='entropy', random_state=0)
+# Fitting Random Forest to the Training set
+from sklearn.ensemble import RandomForestClassifier
+classifier = RandomForestClassifier(n_estimators=100, criterion='entropy')
 classifier.fit(X_train, y_train)
 
 # Predicting the Test set results
@@ -46,7 +44,7 @@ plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
                 c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('Naive Bayes (Training set)')
+plt.title('Random Forest (Training set)')
 plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
 plt.legend()
@@ -64,7 +62,7 @@ plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
                 c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('Naive Bayes (Test set)')
+plt.title('Random Forest (Test set)')
 plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
 plt.legend()
