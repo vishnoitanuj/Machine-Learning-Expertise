aef1494b5f7e5d7f8a0cf4fca86c81e2f20c1c1a
Tanuj Vishnoi <vishnoitanuj@outlook.com>
2018-11-12 00:11:12 +0530

    kmeans complete

--- Part 4 - Clustering/Section 24 - K-Means Clustering/k-means.py
+++ Part 4 - Clustering/Section 24 - K-Means Clustering/k-means.py
@@ -20,3 +20,20 @@ plt.title('The Elbow Method')
 plt.xlabel('Number of custers')
 plt.ylabel('WCSS')
 plt.show()
+
+#Applying K-means to the dataset
+kmeans = KMeans(n_clusters=5, init='k-means++',n_init=10,max_iter=300,random_state=0)
+y_kmeans = kmeans.fit_predict(X)
+
+# Visualizing the clusters
+plt.scatter(X[y_kmeans ==0, 0], X[y_kmeans == 0, 1], s = 50, c= 'red', label = 'Cluster 1')
+plt.scatter(X[y_kmeans ==1, 0], X[y_kmeans == 1, 1], s = 50, c= 'yellow', label = 'Cluster 2')
+plt.scatter(X[y_kmeans ==2, 0], X[y_kmeans == 2, 1], s = 50, c= 'green', label = 'Cluster 3')
+plt.scatter(X[y_kmeans ==3, 0], X[y_kmeans == 3, 1], s = 50, c= 'cyan', label = 'Cluster 4')
+plt.scatter(X[y_kmeans ==4, 0], X[y_kmeans == 4, 1], s = 50, c= 'magenta', label = 'Cluster 5')
+plt.scatter(kmeans.cluster_centers_[:,0], kmeans.cluster_centers_[:,1], s = 150, c= 'black', label = 'Centroids')
+plt.title('Clusters of clients')
+plt.xlabel('Annual Income (k$)')
+plt.ylabel('Spending Score (1-100)')
+plt.legend()
+plt.show()
